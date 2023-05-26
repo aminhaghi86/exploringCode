4b3b0986026190bf1727006a5a5b7491aef381e6
aminhaghi86 <amin.haghi@hyperisland.se>
2023-05-26 16:28:40 +0200

    add frequency and start line from right to left

--- canvas-sinwave/src/app.js
+++ canvas-sinwave/src/app.js
@@ -10,14 +10,18 @@ const wave = {
   y: canvas.height / 2,
   length: 0.01,
   amplitude: 100,
+  frequency: 0.01,
 };
 
 gui.add(wave, "y", 0, canvas.height);
 gui.add(wave, "length", -0.01, 0.01);
 gui.add(wave, "amplitude", -300, 300);
+gui.add(wave, "amplitude", -300, 300);
+gui.add(wave, "frequency", -0.01, 1);
 //
 
 //declare function to recall itself over and over again
+let increament = wave.frequency;
 function animate() {
   requestAnimationFrame(animate);
   ctx.beginPath();
@@ -25,8 +29,12 @@ function animate() {
   ctx.moveTo(0, canvas.height / 2);
 
   for (let i = 0; i < canvas.width; i++) {
-    ctx.lineTo(i, wave.y + Math.sin(i * wave.length) * wave.amplitude);
+    ctx.lineTo(
+      i,
+      wave.y + Math.sin(i * wave.length + increament) * wave.amplitude
+    );
   }
+  increament += wave.frequency;
   ctx.stroke();
 }
 animate();
