65c3fcdf2d1e37b0ee7a91e439a3c07ce2abeced
aminhaghi86 <amin.haghi@hyperisland.se>
2023-05-27 13:01:36 +0200

    install three package and setup scene and camera

---
+++ ThreeJS/src/app.js
@@ -0,0 +1,25 @@
+import * as THREE from "three";
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper);
+
+
+// move the camera position to see ..
+camera.position.z=5
+
+renderer.render(scene, camera);
