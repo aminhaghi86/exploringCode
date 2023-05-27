76174745ca8ffa584c4aba97430dbcb44a524d6c
aminhaghi86 <amin.haghi@hyperisland.se>
2023-05-27 13:25:03 +0200

    plane/material and merge it with geometry in mesh

--- ThreeJS/src/app.js
+++ ThreeJS/src/app.js
@@ -15,38 +15,44 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const orbit = new OrbitControls(camera,renderer.domElement)
+const orbit = new OrbitControls(camera, renderer.domElement);
 
-const axesHelper = new THREE.AxesHelper(3)
+const axesHelper = new THREE.AxesHelper(3);
 scene.add(axesHelper);
 
-
 // move the camera position to see ..
-camera.position.set(0,2,5);
-
+camera.position.set(-10, 30, 30);
 
 //call update method  every time we change position of camera!
-orbit.update()
+orbit.update();
 
 //add box in project
 
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({
-    color:0x00FF00
-})
-const box = new THREE.Mesh(boxGeometry,boxMaterial);
+  color: 0x00ff00,
+});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
+//create plane! pass width and height of plane as a argument to it
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+// create material and merge it with geometry into mesh!
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+
+//then we add it to the scene
 //declare function value of rotation update get increased 60 times per second
 // every second we are going to ratate the cube by 0.01 radians and x and y axis
-function animate (time){
-    box.rotation.x=time/1000;
-    box.rotation.y=time/1000;
-    renderer.render(scene, camera);
+function animate(time) {
+  box.rotation.x = time / 1000;
+  box.rotation.y = time / 1000;
+  renderer.render(scene, camera);
 }
 
-renderer.setAnimationLoop(animate)
-
+renderer.setAnimationLoop(animate);
 
 //
-
